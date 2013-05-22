214e328182f0db2aa0b643dbc6da5b9d993e7912
André M. Ribeiro-dos-Santos <andremrsantos@gmail.com>
2013-05-21 23:03:11 -0300

    Edge complete implementation and documentation, missing specs

--- lib/bass/data/graph/edge.rb
+++ lib/bass/data/graph/edge.rb
@@ -1,19 +1,33 @@
 module Bass::Graph
 
+	# Basic Graph bidirectional edge implementation.
+	# 
+	# @attr_reader weight [Float] 
 	class Edge
 		include Comparable
 
 		attr_reader :weight
 
+		# Basic edge constructor, that represents a bidirectional link between
+		# two nodes.
+		#
+		# @param one   [Object] one of the connected label.
+		# @param other [Object] other of the connected label.
+		# @param weight [Float] the edge weight.
+		# @return self
 		def initialize(one, other, weight = 1.0)
 			@one, @other = (one.hash < other.hash)? [one, other] : [other, one]
 			@weight = weight
 		end
 
+		# @return [Object] one of the nodes linked by the edge.
 		def either
 			@one
 		end
 
+		# @param node [Object] one of the nodes linked by the edge.
+		# @return [Object] the other node linked by the edge.
+		# @raise ArgumentError if the node is not linked by the edge.
 		def other(node)
 			case node
 			when @one then @other
@@ -23,43 +37,66 @@ module Bass::Graph
 			end
 		end
 
+		# @return [Array] set of node linked
 		def nodes
 			[@one, @other]
 		end
 
+		# @return [Integer] an hash number to represent the edge in HashTable
 		def hash
 			@one.hash * 911 + @other.hash * 479
 		end
 
+		# @param other [Object] the object to be compared to self
+		# @return [true, false] if the object is the same class, link the same 
+		# 	nodes and has the same weight
 		def ==(other)
-			other.kind_of? self.class &&
-			nodes  == other.nodes &&
-			weight == other.weight  
+			eql? other && weight == other.weight
 		end
 
-		alias_method :==, :eql?
+		# @param other [Object] the object to be compared to self
+		# @return [true, false] if the object is the same class and link 
+		# 	the same nodes
+		def eql?(other)
+			other.kind_of? self.class &&
+			nodes  == other.nodes
+		end
 
+		# @param other [Edge] the other edge to be compared.
+		# @return [Integer]   the weight comparison: -1 if less, 0 if equal and 
+		# 	1 if more
 		def <=>(other)
 			# TODO: Replace the following for an personalized Error.
-			raise ArgumentError.new('Uncomparable') unless other.kind_of? self.class
+			raise ArgumentError.new('Uncomparable') unless other.kind_of? Edge
 			weight <=> other.weight
 		end
 	end
 
+	# Basic Graph directional edge implementation.
 	class DirectedEdge < Edge
-		def initialize(one, other, weight = 1.0)
-			@one = one
-			@other = other
+		# Basic directional edge constructor, that represents a directional 
+		# link between two nodes.
+		#
+		# @param from [Object] origin node.
+		# @param to   [Object] destination node.
+		# @param weight [Float] the edge weight.
+		# @return self
+		def initialize(from, to, weight = 1.0)
+			@one = from
+			@other = to
 			@weigth = weight
 		end
 
+		# @return [Object] the origin node
 		def from
 			@one
 		end
 
+		# @return [Object] the destination node
 		def to
 			@other
 		end
+		
 	end
 
 end
---
+++ spec/edge_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require 'bass/data/graph'
+
+describe Bass::Graph::Edge do
+end
+
+describe Bass::Graph::DirectedEdge do
+end
