3eb3f082e3ac53832a4a8920ec51bbe91c5a7182
André M. Ribeiro dos Santos <andremrsantos@gmail.com>
2013-05-22 09:03:46 -0300

    Complete spec for Edges

--- lib/bass/data/graph/edge.rb
+++ lib/bass/data/graph/edge.rb
@@ -1,102 +1,111 @@
 module Bass::Graph
 
-	# Basic Graph bidirectional edge implementation.
-	# 
-	# @attr_reader weight [Float] 
-	class Edge
-		include Comparable
+  # Basic Graph bidirectional edge implementation.
+  #
+  # @attr_reader weight [Float]
+  class Edge
+    include Comparable
 
-		attr_reader :weight
+    attr_reader :weight
 
-		# Basic edge constructor, that represents a bidirectional link between
-		# two nodes.
-		#
-		# @param one   [Object] one of the connected label.
-		# @param other [Object] other of the connected label.
-		# @param weight [Float] the edge weight.
-		# @return self
-		def initialize(one, other, weight = 1.0)
-			@one, @other = (one.hash < other.hash)? [one, other] : [other, one]
-			@weight = weight
-		end
+    # Basic edge constructor, that represents a bidirectional link between
+    # two nodes.
+    #
+    # @param one   [Object] one of the connected label.
+    # @param other [Object] other of the connected label.
+    # @param weight [Float] the edge weight.
+    # @return self
+    def initialize(one, other, weight = 1.0)
+      @one, @other = (one.hash < other.hash)? [one, other] : [other, one]
+      @weight = weight
+    end
 
-		# @return [Object] one of the nodes linked by the edge.
-		def either
-			@one
-		end
+    # @return [Object] one of the nodes linked by the edge.
+    def either
+      @one
+    end
 
-		# @param node [Object] one of the nodes linked by the edge.
-		# @return [Object] the other node linked by the edge.
-		# @raise ArgumentError if the node is not linked by the edge.
-		def other(node)
-			case node
-			when @one then @other
-			when @other then @one
-			# TODO: Replace the following for an personalized Error.
-			else raise ArgumentError.new('Invalid vertex')
-			end
-		end
+    # @param node [Object] one of the nodes linked by the edge.
+    # @return [Object] the other node linked by the edge.
+    # @raise ArgumentError if the node is not linked by the edge.
+    def other(node)
+      case node
+      when @one then @other
+      when @other then @one
+      # TODO: Replace the following for an personalized Error.
+      else raise ArgumentError.new('Invalid vertex')
+      end
+    end
 
-		# @return [Array] set of node linked
-		def nodes
-			[@one, @other]
-		end
+    # @return [Array] set of node linked
+    def nodes
+      [@one, @other]
+    end
 
-		# @return [Integer] an hash number to represent the edge in HashTable
-		def hash
-			@one.hash * 911 + @other.hash * 479
-		end
+    # @return [Integer] an hash number to represent the edge in HashTable
+    def hash
+      @one.hash * 911 + @other.hash * 479
+    end
 
-		# @param other [Object] the object to be compared to self
-		# @return [true, false] if the object is the same class, link the same 
-		# 	nodes and has the same weight
-		def ==(other)
-			eql? other && weight == other.weight
-		end
+    # @param other [Object] the object to be compared to self
+    # @return [true, false] if the object is the same class, link the same
+    #   nodes and has the same weight
+    def ==(other)
+      eql?(other) && weight == other.weight
+    end
 
-		# @param other [Object] the object to be compared to self
-		# @return [true, false] if the object is the same class and link 
-		# 	the same nodes
-		def eql?(other)
-			other.kind_of? self.class &&
-			nodes  == other.nodes
-		end
+    # @param other [Object] the object to be compared to self
+    # @return [true, false] if the object is the same class and link
+    #   the same nodes
+    def eql?(other)
+      other.kind_of?(self.class) && self.nodes  == other.nodes
+    end
 
-		# @param other [Edge] the other edge to be compared.
-		# @return [Integer]   the weight comparison: -1 if less, 0 if equal and 
-		# 	1 if more
-		def <=>(other)
-			# TODO: Replace the following for an personalized Error.
-			raise ArgumentError.new('Uncomparable') unless other.kind_of? Edge
-			weight <=> other.weight
-		end
-	end
+    # @param other [Edge] the other edge to be compared.
+    # @return [Integer]   the weight comparison: -1 if less, 0 if equal and
+    #   1 if more
+    def <=>(other)
+      # TODO: Replace the following for an personalized Error.
+      raise ArgumentError.new('Uncomparable') unless other.kind_of? Edge
+      weight <=> other.weight
+    end
 
-	# Basic Graph directional edge implementation.
-	class DirectedEdge < Edge
-		# Basic directional edge constructor, that represents a directional 
-		# link between two nodes.
-		#
-		# @param from [Object] origin node.
-		# @param to   [Object] destination node.
-		# @param weight [Float] the edge weight.
-		# @return self
-		def initialize(from, to, weight = 1.0)
-			@one = from
-			@other = to
-			@weigth = weight
-		end
+    # @return [String] the edge representation
+    def to_s
+      '(%s, %s : %d)' % [@one, @other, weight]
+    end
 
-		# @return [Object] the origin node
-		def from
-			@one
-		end
+    # @return (see #to_s)
+    def inspect
+      to_s
+    end
+  end
 
-		# @return [Object] the destination node
-		def to
-			@other
-		end
-		
-	end
+  # Basic Graph directional edge implementation.
+  class DirectedEdge < Edge
+    # Basic directional edge constructor, that represents a directional
+    # link between two nodes.
+    #
+    # @param from [Object] origin node.
+    # @param to   [Object] destination node.
+    # @param weight [Float] the edge weight.
+    # @return self
+    def initialize(from, to, weight = 1.0)
+      @one = from
+      @other = to
+      @weigth = weight
+    end
+
+    # @return [Object] the origin node
+    def from
+      @one
+    end
+
+    # @return [Object] the destination node
+    def to
+      @other
+    end
+
+  end
 
 end
--- spec/edge_spec.rb
+++ spec/edge_spec.rb
@@ -2,7 +2,42 @@ require 'rspec'
 require 'bass/data/graph'
 
 describe Bass::Graph::Edge do
+  let(:edge_a) { Bass::Graph::Edge.new(1, 2, 10) }
+  let(:edge_b) { Bass::Graph::Edge.new(2, 1, 5) }
+
+  it 'is comparable' do
+    edge_a.should_not == edge_b
+    edge_a.should > edge_b
+  end
+
+  it 'recognize edges' do
+    edge_a.other(1).should == 2
+    edge_a.other(2).should == 1
+  end
+
+  it 'is hashable' do
+    hash = { edge_a => 0 }
+    hash[edge_b].should == 0
+  end
+
 end
 
 describe Bass::Graph::DirectedEdge do
+  let(:edge_a) { Bass::Graph::DirectedEdge.new(1, 2) }
+  let(:edge_b) { Bass::Graph::DirectedEdge.new(2, 1) }
+
+  it 'diferenciate source and destiny' do
+    edge_a.should_not == edge_b
+  end
+
+  it 'has source and destiny' do
+    edge_a.from.should == 1
+    edge_a.to.should == 2
+  end
+
+  it 'is hashable' do
+    hash = { edge_a => 0 }
+    hash[edge_b].should_not == 0
+  end
+
 end
